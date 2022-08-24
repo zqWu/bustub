de993daec2b34c1c24beeeba9d4b74b3654e6460
dormi330 <dormi330@gmail.com>
2022-08-24 15:41:41 +0800

    project1 - task1 tested

--- src/buffer/clock_replacer.cpp
+++ src/buffer/clock_replacer.cpp
@@ -11,19 +11,209 @@
 //===----------------------------------------------------------------------===//
 
 #include "buffer/clock_replacer.h"
+#include <climits>
 
 namespace bustub {
 
-ClockReplacer::ClockReplacer(size_t num_pages) {}
+ClockReplacer::ClockReplacer(size_t num_pages) {
+  capacity_ = num_pages;
+  pin_size_ = 0;
+  clock_size_ = 0;
+}
 
 ClockReplacer::~ClockReplacer() = default;
 
-auto ClockReplacer::Victim(frame_id_t *frame_id) -> bool { return false; }
+/**
+ * case empty => false
+ * case all pin => false
+ * case all refer=1 => min frame_id
+ * other: from current_ to first node which refer=0
+ */
+auto ClockReplacer::Victim(frame_id_t *frame_id) -> bool {
+  if (head_ == nullptr) {
+    return false;
+  }
 
-void ClockReplacer::Pin(frame_id_t frame_id) {}
+  size_t cnt = 0;
+  ListNode *target = nullptr;
+  ListNode *pre = nullptr;
+  ListNode *min_frame = nullptr;
+  frame_id_t min_frame_id = INT_MAX;
 
-void ClockReplacer::Unpin(frame_id_t frame_id) {}
+  // 从current开始, 查找 victim节点
+  while (cnt < clock_size_) {
+    if (current_ == nullptr) {
+      break;
+    }
 
-auto ClockReplacer::Size() -> size_t { return 0; }
+    if (current_->pin_) {
+      // just skip
+      current_ = current_->next_;
+      cnt++;
+      continue;
+    }
+
+    // 如果该节点 !refer
+    if (!current_->refer_) {
+      target = current_;
+      break;  // found
+    }
+
+    // not pin and refer
+    current_->refer_ = false;
+    if (current_->frame_id_ < min_frame_id) {
+      min_frame_id = current_->frame_id_;
+      min_frame = current_;
+    }
+
+    pre = current_;
+    current_ = current_->next_;
+    cnt++;
+  }
+
+  if (target == nullptr && min_frame != nullptr) {
+    target = min_frame;
+  }
+
+  if (target != nullptr) {
+    // 处理指针
+    if (clock_size_ == 1) {
+      // last one
+      head_ = nullptr;
+      tail_ = nullptr;
+      current_ = nullptr;
+    } else {
+      if (target == head_) {
+        head_ = target->next_;
+        tail_->next_ = head_;
+      } else {
+        if (pre == nullptr) {
+          pre = head_;
+        }
+        pre->next_ = target->next_;
+      }
+    }
+
+    if (current_ == target) {
+      current_ = target->next_;
+    }
+
+    *frame_id = target->frame_id_;
+
+    // 处理
+    target->next_ = nullptr;
+    free(target);
+    clock_size_--;
+    return true;
+  }
+
+  return false;
+}
+
+void ClockReplacer::Pin(frame_id_t frame_id) {
+  // if empty then add
+  if (clock_size_ == 0) {
+    return;
+  }
+
+  // first search, if not exist then add
+  size_t cnt = 0;
+  ListNode *node = head_;
+  ListNode *target = nullptr;
+
+  while (cnt < clock_size_) {
+    if (node == nullptr) {
+      break;
+    }
+
+    // check this node
+    if (node->frame_id_ == frame_id) {
+      target = node;
+      break;
+    }
+
+    // not this node
+    node = node->next_;
+    cnt++;
+  }
+
+  // found
+  if (target != nullptr) {
+    target->pin_ = true;
+    target->refer_ = true;
+    pin_size_++;
+  }
+
+  // not found, do nothing
+}
+
+void ClockReplacer::Unpin(frame_id_t frame_id) {
+  // if empty then add
+  if (clock_size_ == 0) {
+    head_ = static_cast<ListNode *>(Create_node(frame_id, false, nullptr));
+    head_->next_ = head_;
+    current_ = head_;
+    return;
+  }
+
+  // first search, if not exist then add
+  size_t cnt = 0;
+  ListNode *node = head_;
+  ListNode *target = nullptr;
+  ListNode *pre = nullptr;
+
+  while (cnt < clock_size_) {
+    if (node == nullptr) {
+      break;
+    }
+
+    // check this node
+    if (node->frame_id_ == frame_id) {
+      target = node;
+      break;
+    }
+
+    // not this node
+    pre = node;
+    node = node->next_;
+    cnt++;
+  }
+
+  // found
+  if (target != nullptr) {
+    printf("unpin found duplicate %d", frame_id);
+    if (target->pin_) {
+      target->pin_ = false;
+      pin_size_--;
+    }
+    return;
+  }
+
+  // not found
+  if (clock_size_ < capacity_) {
+    // case: not full
+    ListNode *new_node = static_cast<ListNode *>(Create_node(frame_id, false, head_));
+    pre->next_ = new_node;
+    tail_ = new_node;
+  } else {
+    // full, we should evacuate one element
+    throw "TODO not implement";
+  }
+}
+
+auto ClockReplacer::Size() -> size_t { return clock_size_ - pin_size_; }
+
+void *ClockReplacer::Create_node(frame_id_t frame_id, bool pin, void *next) {
+  ListNode *new_node = static_cast<ListNode *>(malloc(sizeof(ListNode)));
+  new_node->pin_ = pin;
+  new_node->frame_id_ = frame_id;
+  new_node->refer_ = true;
+  new_node->next_ = static_cast<ListNode *>(next);
+
+  // increase size
+  clock_size_++;
+
+  return new_node;
+}
 
 }  // namespace bustub
--- src/include/buffer/clock_replacer.h
+++ src/include/buffer/clock_replacer.h
@@ -37,16 +37,35 @@ class ClockReplacer : public Replacer {
    */
   ~ClockReplacer() override;
 
+  // 查找下一个 victim
   auto Victim(frame_id_t *frame_id) -> bool override;
 
+  // 该 frame_id必须存在
   void Pin(frame_id_t frame_id) override;
 
   void Unpin(frame_id_t frame_id) override;
 
   auto Size() -> size_t override;
 
+  void *Create_node(frame_id_t frame_id, bool pin, void *next);
+
  private:
   // TODO(student): implement me!
+  struct ListNode {
+    bool pin_;
+    bool refer_;
+    frame_id_t frame_id_;
+    ListNode *next_;
+  };
+
+  size_t capacity_;
+  size_t clock_size_;
+  size_t pin_size_;
+
+  ListNode *head_;
+  ListNode *tail_;
+  ListNode *current_;
+
 };
 
 }  // namespace bustub
